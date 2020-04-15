e8a324f53cfd0be8211ca41d469217ee2895300d
sihanwang <ocpwj@hotmail.com>
2020-04-15 13:35:28 +0800

    init

---
+++ src/main/java/com/sihan/study/SpringJavaConfig/SpringJavaConfigApplication.java
@@ -0,0 +1,13 @@
+package com.sihan.study.SpringJavaConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihan/study/SpringJavaConfig/SpringJavaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.SpringJavaConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
